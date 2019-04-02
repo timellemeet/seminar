bd9ac52c0a882b390d0955c7d25c336e11db7e59
Daniël <danieldbondt@gmail.com>
2019-04-02 11:02:56 +0200

    Activation class introduced that allows additition of alpha parameter

--- src/neuralnet/main.py
+++ src/neuralnet/main.py
@@ -11,7 +11,7 @@ from performance_func import plot_error, plot_confusion_matrix
 from network_queue import Queue
 np.random.seed(10)
 # import data
-training_size = 6000
+training_size = 3000
 normalize = True
 training, labels, test, original_test_labels, test_labels = import_data(size=training_size, normalize=normalize)
 
@@ -29,8 +29,9 @@ max_epochs = 5
 batch_size = 1
 weight_decay = 0.01
 momentum = True
+relu_alpha = 0.5
 
-architectures = [[30],[100]]
+architectures = [[30]]
 for layers in architectures:
     queue.add(netparams={
         "hidden_layers": layers,
@@ -38,19 +39,21 @@ for layers in architectures:
         "output_classes": output_classes,
         "activation": relu,
         "activation_prime": relu_prime,
+        "activation_alpha": relu_alpha,
         "loss_activation": softmax,
         "loss_activation_prime": softmax_prime,
         "loss": cross_entropy,
         "loss_prime": cross_entropy_prime
-    },
-    folds=5,
-    params={"epochs": 5,
-            "learning_rate": 5e-3,
-            "batch_size": 1,
-            "momentum": False,
-            "weight_decay": 0.01})
+        },
+        folds=5,
+        params={"epochs": 5,
+                "learning_rate": 5e-3,
+                "batch_size": 1,
+                "momentum": False,
+                "weight_decay": 0.01},
+        description="architecture: "+str(layers)+" training_size: "+str(training_size))
 
-results, accuracies = queue.execute()
+results_queue = queue.execute()
 
 # # prepare data for training by selecting validation set
 # fold_train_data, fold_train_labels, fold_val_data, fold_val_labels = k_fold(training, labels, k=5, n=5)
@@ -60,10 +63,10 @@ results, accuracies = queue.execute()
 #                                        max_epochs, learning_rate, batch_size, momentum, weight_decay)
 
 # train the model on training data and labels using specific hyper-parameters
-errors, val_errors, val_accs = net.fit(fold_train_data, fold_train_labels, fold_val_data, fold_val_labels,
-                             max_epochs, learning_rate, batch_size, momentum, weight_decay)
-net.save_parameters("%s HU, %f LR, %d epochs, %d batchsize, %f weightdecay, %g momentum" %(hidden_layers, learning_rate, max_epochs, batch_size, weight_decay, momentum))
-# print the accuracy
+# errors, val_errors, val_accs = net.fit(fold_train_data, fold_train_labels, fold_val_data, fold_val_labels,
+#                              max_epochs, learning_rate, batch_size, momentum, weight_decay)
+# net.save_parameters("%s HU, %f LR, %d epochs, %d batchsize, %f weightdecay, %g momentum" %(hidden_layers, learning_rate, max_epochs, batch_size, weight_decay, momentum))
+# # print the accuracy
 # print("The test accuracy of the network is: {}".format(
 #     net.accuracy(x=test, y_true=original_test_labels, errors=errors, val_errors=val_errors)))
 
@@ -72,5 +75,5 @@ net.save_parameters("%s HU, %f LR, %d epochs, %d batchsize, %f weightdecay, %g m
 #                       normalize=True,
 #                       title='Normalized confusion matrix')
 # plt.show()
-plot_error(accuracies[:5], accuracies[5:10])
+# plot_error(accuracies[:5], accuracies[5:10])
 
--- src/neuralnet/network.py
+++ src/neuralnet/network.py
@@ -13,17 +13,17 @@ class Network:
                   features, output_classes,
                   activation, activation_prime,
                   loss_activation, loss_activation_prime,
-                  loss, loss_prime):
+                  loss, loss_prime, activation_alpha=None):
         self.layers = []
         self.loss = None
         self.loss_prime = None
         # fill it with several layers
         self.add(FCLayer(features, hidden_layers[0]))
-        self.add(ActivationLayer(ActivationFunction(activation, activation_prime)))
+        self.add(ActivationLayer(ActivationFunction(activation, activation_prime, activation_alpha)))
 
         for i in range(1, len(hidden_layers)):
             self.add(FCLayer(hidden_layers[i - 1], hidden_layers[i]))
-            self.add(ActivationLayer(ActivationFunction(activation, activation_prime)))
+            self.add(ActivationLayer(ActivationFunction(activation, activation_prime, activation_alpha)))
 
         self.add(FCLayer(hidden_layers[-1], output_classes))
         self.add(LossLayer(loss_activation, loss_activation_prime, loss, loss_prime))
